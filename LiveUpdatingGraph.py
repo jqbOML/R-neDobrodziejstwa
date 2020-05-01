75d235224953a30b8e505b027df3d6d7e38e5859
Jakub Omieljaniuk <56368562+jqbOML@users.noreply.github.com>
2020-05-01 05:25:58 +0200

    Update LiveUpdatingGraph.py

--- LiveUpdatingGraph.py
+++ LiveUpdatingGraph.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
@@ -10,7 +9,7 @@ def animate(i):
     lines = data.split('\n')
     xs = []
     ys = []
-   
+	
     for line in lines:
         x, y = line.split(',')    
         xs.append(float(x))
@@ -22,7 +21,5 @@ def animate(i):
     plt.xlabel('x_label_name')
     plt.ylabel('y_label_name')
     plt.title('Live graph with matplotlib')	
-	
-    
 ani = animation.FuncAnimation(fig, animate, interval=1000) 
 plt.show()
